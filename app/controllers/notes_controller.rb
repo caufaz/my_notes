220b2f07b0e3dab395cbca301c9d6726a027bbec
caufaz <gildas.pintaud@unitak.fr>
2016-05-11 17:34:06 +0200

    recup parametre type get et affichage message dans la vue

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,5 +2,7 @@ class NotesController < ApplicationController
   def index
   	puts "Hello"
   	#render :index permet de choisir le template
+  	puts params[:message]
+  	@message = params[:message]
   end
 end
